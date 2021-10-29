fc6c4e137bd2450477b4d2a64a215fb69e6345b4
hezhi <hezhi@joyy.com>
2021-10-30 01:37:57 +0800

    test

--- 21215122_hezhi_cv/hezhi/main.cpp
+++ 21215122_hezhi_cv/hezhi/main.cpp
@@ -2,24 +2,31 @@
 
 int main()
 {
-	CImg<unsigned char> bigben("../test_Data/bigben.jpg");
 	Canny c;
-	CImg<unsigned char> bigben_canny = c.build(bigben, bigben.width(), bigben.height());
-	// bigben.save("../result_Data/bigben_canny.bmp");
-    bigben.display("../result_Data/bigben_canny.bmp");
 
-	// CImg<unsigned char> lena_grey("../test_Data/lena.bmp");
-	// CImg<unsigned char> lena_canny = c.canny(lena_grey, lena_grey.width(), lena_grey.height());
-	// lena_canny.save("../result_Data/lena_canny.bmp");
-	// //lena_canny.display("lena_canny");
+	CImg<unsigned char> lena("../test_Data/lena.jpg");
+	CImg<unsigned char> lenaCanny = c.build(lena, lena.width(), lena.height());
+	lenaCanny.save("./result_Data/lena_canny.jpg");
 
-	// CImg<unsigned char> stpietro_grey("../test_Data/stpietro.bmp");
-	// CImg<unsigned char> stpietro_canny = c.canny(stpietro_grey, stpietro_grey.width(), stpietro_grey.height());
-	// stpietro_canny.save("../result_Data/stpietro_canny.bmp");
+	CImg<unsigned char> bigben("../test_Data/bigben.jpg");
+	CImg<unsigned char> bigbenCanny = c.build(bigben, bigben.width(), bigben.height());
+	bigbenCanny.save("./result_Data/bigben_canny.jpg");
 
-	// CImg<unsigned char> twows_grey("../test_Data/twows.bmp");
-	// CImg<unsigned char> twows_canny = c.canny(twows_grey, twows_grey.width(), twows_grey.height());
-	// twows_canny.save("../result_Data/twows_canny.bmp");
+	CImg<unsigned char> stpietro("../test_Data/stpietro.jpg");
+	CImg<unsigned char> stpietroCanny = c.build(stpietro, stpietro.width(), stpietro.height());
+	stpietroCanny.save("./result_Data/stpietro_canny.jpg");
+
+	CImg<unsigned char> image1("../test_Data/3.jpg");
+	CImg<unsigned char> image1Canny = c.build(image1, image1.width(), image1.height());
+	image1Canny.save("./result_Data/3_canny.jpg");
+
+	CImg<unsigned char> image2("../test_Data/4.jpg");
+	CImg<unsigned char> image2Canny = c.build(image2, image2.width(), image2.height());
+	image2Canny.save("./result_Data/4_canny.jpg");
+
+	CImg<unsigned char> image3("../test_Data/20160326110137505.JPG");
+	CImg<unsigned char> image3Canny = c.build(image3, image3.width(), image3.height());
+	image3Canny.save("./result_Data/20160326110137505_canny.jpg");
 
 	return 0;
 }
