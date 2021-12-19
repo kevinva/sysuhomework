a88177681cdd2944dde11e8d85ec52d6a1196fee
hezhi <hezhi@yy.com>
2021-12-20 00:11:33 +0800

    Test

---
+++ 模式识别/21215122_final/tmp/test1.c
@@ -0,0 +1,4 @@
+void spapr_tce_set_bypass(sPAPRTCETable *tcet, bool bypass)
+{
+    tcet->bypass = bypass;
+}
---
+++ 模式识别/21215122_final/tmp/test3.c
@@ -0,0 +1,8 @@
+int myFunc(int i, int *a, int c)
+{
+    while(i > 10) {
+        i++;
+    }
+
+    return i + c
+}
