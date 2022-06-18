76c5c65e092bc146d269c003f59e41d22a76e4ee
hezhi <hezhi@yy.com>
2022-06-18 20:21:20 +0800

    最优化期末done

--- 最优化理论与方法/final_exam/code/main.py
+++ 最优化理论与方法/final_exam/code/main.py
@@ -1,4 +1,4 @@
-from numpy import power
+import numpy as np
 import torch
 
 N_MAX = 50
@@ -26,7 +26,7 @@ def powerIteration(m):
 
     return eig_vec, eig_val
 
-
+# 获得Householder矩阵
 def getHouseholderMatrix(vec):
     n = vec.size()[0]
     I = torch.eye(n)
@@ -39,41 +39,57 @@ def getHouseholderMatrix(vec):
     return H
 
 
-def getAll(mat):
+def main(mat):
     n = mat.size()[0]
     eig_vec_list = []
     eig_val_list = []
-    eig_vec, eig_val = powerIteration(mat)
-    eig_vec_list.append(eig_vec)
-    eig_val_list.append(eig_val)
-    H = getHouseholderMatrix(eig_vec)
-    A = torch.matmul(torch.matmul(H, mat), H.mT)   # 用Householder矩阵对原始矩阵做正交相似变换
-    for i in range(n - 1):
-        r1 = A[0, 1:]
-        mat = A[1:, 1:]
-
-
-
+    H_list = []
+    r1_list = []
+    evectmp_list = []
+    for i in range(n):
+        print(f'epoch: {i+1} ==========================================')
         eig_vec, eig_val = powerIteration(mat)
-        eig_vec_list.append(eig_vec)
         eig_val_list.append(eig_val)
-        H = getHouseholderMatrix(eig_vec)
-        A = torch.matmul(torch.matmul(H, mat), H.mT)  
 
+        if i == 0:
+            eig_vec_list.append(eig_vec)
+        else:
+            evectmp_list.append(eig_vec)
+            eval_idx = len(eig_val_list) - 1
+            for H_mat, r1_vec, evectmp in zip(reversed(H_list), reversed(r1_list), reversed(evectmp_list)):
+                alpha = r1_vec.dot(evectmp) / (eig_val_list[eval_idx] - eig_val_list[eval_idx - 1])
+                alpha = alpha.unsqueeze(0)
+                tmp_vec = torch.cat((alpha, evectmp)).view(-1, 1)
+                final_vec = torch.matmul(H_mat, tmp_vec)
+
+        H = getHouseholderMatrix(eig_vec) # 获得Householder矩阵
+        A = torch.matmul(torch.matmul(H, mat), H.mT)  # 用Householder矩阵对原始矩阵做正交相似变换
+
+        mat = A[1:, 1:]
+        r1 = A[0, 1:]
+        H_list.append(H)
+        r1_list.append(r1)
+        
+        if mat.size()[0] == 1:
+            eig_val_list.append(mat[0][0])
+            break
+    
+    return torch.stack(eig_val_list).numpy(), eig_vec_list
 
 
 if __name__ == '__main__':
-    mat = torch.rand((3, 3))
-    # mat = torch.tensor([[3, -1, 1], [2, 0, 1], [1, -1, 2]], dtype=torch.float32)
+    # mat = torch.rand((10, 10))
+    mat = torch.tensor([[3, -1, 1], [2, 0, 1], [1, -1, 2]], dtype=torch.float32)
     print(f'mat: {mat}')
-    eig_vec, eig_val = powerIteration(mat)
+    eig_vals, eig_vecs = main(mat)
+    print(f'eig_vals={eig_vals}')
 
-    evals, evecs = torch.eig(mat, eigenvectors=True)
-    print(evals)
-    print(evecs)
+    # test_evals, test_evecs = torch.eig(mat, eigenvectors=True)
+    # print(f'test_evals: {test_evals}')
+    # print(f'test_evects: {test_evecs}')
 
-    H = getHouseholderMatrix(eig_vec)
-    A = torch.matmul(torch.matmul(H, mat), H.mT)
-    print(A)
-    print(A[0, 1:])
-    print(A[2:, 2:])
+    test2_evals, test2_evecs = np.linalg.eig(mat)
+    print(f'test2_evals: {test2_evals}')
+    print(f'test2_evecs: {test2_evecs}')
+
+
